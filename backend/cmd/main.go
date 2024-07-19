76d045dfdf6e1f3ad017fdb930f11c97e67cca8f
AntJamGeo <57462383+AntJamGeo@users.noreply.github.com>
2024-07-19 11:19:13 +0100

    Create landing page

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"path/filepath"
 
+	"github.com/AntJamGeo/go-tic-tac-toe/backend/internal/utils"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir(filepath.Join(utils.Root, "frontend"))))
+	log.Fatalf("stopped listening: %s", http.ListenAndServe(":3000", nil))
 }
